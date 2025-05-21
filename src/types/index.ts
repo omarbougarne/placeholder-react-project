953feae8bd5b6280e7f2221c66f25238ca7b295e
omarbougarne <omarbougarne98@gmail.com>
2025-05-21 16:09:50 +0100

    init

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,47 @@
-export interface Item {
+export interface Post {
     id: number;
     title: string;
-    content: string;
+    body: string;
+    userId: number;
+}
+
+export interface Comment {
+    id: number;
+    postId: number;
+    name: string;
+    email: string;
+    body: string;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        }
+    };
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    }
 }
 
 export interface ApiResponse<T> {
     data: T;
     error?: string;
 }
+
+
+export type CreatePostPayload = Omit<Post, 'id'>;
+export type UpdatePostPayload = Partial<CreatePostPayload>;
